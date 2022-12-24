a58aa569be83dfcada6b4bc213bf69d303bb95f3
Alice <nedumayu@gmail.com>
2022-12-25 02:02:32 +0300

    add all about reports

--- src/main/java/ru/project/schoolInfoSystem/model/Report.java
+++ src/main/java/ru/project/schoolInfoSystem/model/Report.java
@@ -5,11 +5,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Date;
 
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Report {
-//
-//}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Report {
+    private Long id;
+    private Date reportDate;
+    private int studentsCount;
+    private int teachersCount;
+    private int classroomCount;
+    private int classesCount;
+    private float averageGrades;
+    private int excellentCount;
+    private int goodCount;
+    private int failureCount;
+    private int underachieverCount;
+}
